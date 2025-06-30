6a36fa5263dc7e27b807dad6cd8ee38047c83c5f
OH-Mia <song143486@gmail.com>
2025-06-30 17:38:45 +0900

    ✨ [Feat](app): app 하위 head 정보 업데이트 #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,26 @@ export default defineNuxtConfig({
     typeCheck: false,
   },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'Mia Mia',
+      viewport: 'width=device-width, initial-scale=1',
+      charset: 'utf-8',
+      htmlAttrs: {
+        lang: 'ko',
+      },
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/kkamgo.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/kkamgo.png',
+          sizes: '180x180',
+        },
+      ],
+    },
+  },
 })
