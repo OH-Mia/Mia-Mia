cf28b9d4f40c3a10ac90096d024e8751586dffe4
OH-Mia <song143486@gmail.com>
2025-07-02 11:23:08 +0900

    🚧 [Build](app): 피니아, dayjs, 아이콘 관련 라이브러리 추가 설치 및 관련 설정 #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,51 @@
 // nuxt.config.ts
+import { env } from 'node:process'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: '2025-06-30',
   typescript: {
     typeCheck: false,
   },
+  modules: [
+    '@element-plus/nuxt',
+    '@nuxt/icon',
+    ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }],
+  ],
+  elementPlus: {
+    themes: ['dark'],
+    icon: 'ElIcon',
+    importStyle: 'css',
+  },
+  imports: {
+    dirs: [
+      'stores',
+      'composables',
+      'components',
+    ],
+  },
   css: ['~/assets/css/main.css'],
+  nitro: {
+    devProxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
+  vite: {
+    optimizeDeps: {
+      include: ['dayjs'],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+        },
+      },
+    },
+  },
   app: {
     head: {
       title: 'Mia Mia',
@@ -26,6 +66,11 @@ export default defineNuxtConfig({
           sizes: '180x180',
         },
       ],
+    },
+  },
+  runtimeConfig: {
+    public: {
+      youtubeUrl: env.VUE_APP_YOUTUBE,
     },
   },
 })
