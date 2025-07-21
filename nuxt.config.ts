86d51203d312e71d81a70e3bacc28f3d578822e3
OH-Mia <song143486@gmail.com>
2025-07-21 11:11:08 +0900

    ✨ [Feat](app): 파비콘 이미지 설정을 위한 파일 추가 및 설정 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -87,6 +87,13 @@ export default defineNuxtConfig({
           href: '/kkamgo.png',
           sizes: '180x180',
         },
+        { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }, // 선택 사항
+      ],
+      meta: [
+        { name: 'theme-color', content: '#ffffff' },
       ],
     },
   },
