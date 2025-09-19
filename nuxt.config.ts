835320f857b36060c67758a788cb090e878f4dc6
OH-Mia <song143486@gmail.com>
2025-09-19 11:09:50 +0900

    ✨ [Feat](app): 파비콘 이미지 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -98,16 +98,16 @@ export default defineNuxtConfig({
         {
           rel: 'icon',
           type: 'image/png',
-          href: '/kkamgo.png',
+          href: '/favicon.png',
         },
         {
           rel: 'apple-touch-icon',
-          href: '/kkamgo.png',
+          href: '/favicon.png',
           sizes: '180x180',
         },
-        { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
-        { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.png', sizes: '16x16' },
+        { rel: 'apple-touch-icon', href: '/favicon.png.png' },
         { rel: 'manifest', href: '/site.webmanifest' }, // 선택 사항
       ],
       meta: [
